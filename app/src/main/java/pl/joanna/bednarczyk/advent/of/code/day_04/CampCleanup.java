7cd3709bc8d2ab01c2c5deaa86c0d780ee3696b5
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2022-12-04 23:27:36 +0100

    Day 4 Part 2 completed

--- app/src/main/java/pl/joanna/bednarczyk/advent/of/code/day_04/CampCleanup.java
+++ app/src/main/java/pl/joanna/bednarczyk/advent/of/code/day_04/CampCleanup.java
@@ -6,14 +6,16 @@ import java.util.*;
 
 public class CampCleanup {
     private static final String INPUT_PATH = "input/day_04.txt";
+    private static final List<Pair<String, String>> PAIR_RANGES = getPairRanges();
     private int numberOfFullyContained;
     private int numberOfOverlaps;
 
     public CampCleanup(){
-        this.numberOfFullyContained = countFullyContainedRanges(getPairRanges());
+        this.numberOfFullyContained = countFullyContainedRanges(PAIR_RANGES);
+        this.numberOfOverlaps = countOverlaps(PAIR_RANGES);
     }
 
-    private List<Pair<String, String>> getPairRanges(){
+    private static List<Pair<String, String>> getPairRanges(){
         FileReader reader = new FileReader(INPUT_PATH);
         List<String> inputFromFile = reader.getInputFromFile();
         List<Pair<String, String>> pairs = new ArrayList<>();
@@ -38,6 +40,29 @@ public class CampCleanup {
         }
 
         return sum;
+    }
+
+    private int countOverlaps(List<Pair<String, String>> pairList){
+        int sum = 0;
+        for(Pair<String, String> pair : pairList){
+            String firstRange = pair.getFirst();
+            String secondRange = pair.getSecond();
+            if(isOverlapWithinRange(firstRange, secondRange)){
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+
+    private boolean isOverlapWithinRange(String firstRange, String secondRange){
+        Pair<Integer, Integer> firstAsNumbers = transformRangeToNumericValues(firstRange);
+        Pair<Integer, Integer> secondAsNumbers = transformRangeToNumericValues(secondRange);
+
+        return (firstAsNumbers.getSecond() >= secondAsNumbers.getFirst() &&
+                firstAsNumbers.getFirst() <= secondAsNumbers.getSecond()) ||
+                (secondAsNumbers.getSecond() >= firstAsNumbers.getFirst() &&
+                        secondAsNumbers.getFirst() <= firstAsNumbers.getSecond());
     }
 
     private boolean isRangeFullyContainedByOther(String firstRange, String secondRange){
